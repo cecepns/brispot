fda7acea73bca39eed832fa3c702407c32e74c80
Cecep Nandang <cecep@kima.one>
2025-09-25 15:39:35 +0700

    fix: brispot

--- src/components/InputDataPage.jsx
+++ src/components/InputDataPage.jsx
@@ -233,12 +233,12 @@ export default function InputDataPage() {
               Revisi
             </button>
           </div>
-          <button
+          {/* <button
             onClick={() => window.location.href = '/list-data'}
             className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg transition-colors"
           >
             Lihat Data
-          </button>
+          </button> */}
         </div>
 
         {loadingData && (
